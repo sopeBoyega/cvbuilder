faa9e6f074a399cb6767c9d981323d8925b5bb70
s._.ope <sopeadegboyega@gmail.com>
2025-02-07 01:33:34 +0100

    Hero Section Done

---
+++ app/fonts/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter } from "next/font/google";
+import { Noto_Sans } from "next/font/google";
+import { Outfit } from "next/font/google";
+
+export const outfit = Outfit({ subsets: ["latin"] });
+export const notoSans = Noto_Sans({ subsets: ["latin"] });
+
+export const inter = Inter({ subsets: ["latin"] });
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,73 @@
 import Image from "next/image";
+import { notoSans } from "./fonts/fonts";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="min-w-full min-h-full bg-white">
+      {/* Hero Section */}
+      <div className="w-full h-full bg-[url(/hero.png)] flex items-start justify-center relative">
+        {/* Navbar */}
+        <div className="w-[90%] h-[50px]  flex justify-between items-center absolute z-50 ">
+          <div className="">
+            <p className={`${notoSans.className} `}>CV Builder</p>
+          </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <div className="w-fit h-fit flex flex-row gap-2  justify-center items-center">
+            <p className={`${notoSans.className} `}>Sign in</p>
+            <button className="border-[1.5px] rounded-[15px] py-[6.5px] px-[11.5px] border-[#D0D7DE]">
+              <p>Sign Up</p>
+            </button>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <Image
+          src="/hero.png"
+          width={0}
+          height={0}
+          layout="responsive"
+          alt="Image"
+        />
+        <Image
+          src="/branch.svg"
+          width={350}
+          height={1000}
+          alt="Image"
+          className="absolute left-[3vw] top-[15vh]"
+        />
+
+        <div className="w-[80%] h-[500px]  absolute left-[8vw]  bottom-[4rem] flex flex-col items-start justify-start">
+          {/* CV tab */}
+          <div className="w-fit h-fit scale-[1.0] bottom-0 gap-5 rounded-full py-[5px] px-[5px] mt-[-10px] bg-transparent border-[1.5px] border-[#424B5B] flex flex-row justify-center  items-center ">
+            <Image src="/cv.svg" width={50} height={50} alt="Image" />
+            <div className="flex flex-col">
+              <p>CV Builder Hub: Craft Your Professional Identity</p>
+              <p className="text-[#6E7681] text-[15px]">
+                Create your CV effortlessly—completely free!
+              </p>
+            </div>
+          </div>
+
+          <p className="font-[700] text-[50px] text-white">
+            Start Your <br />Professional Journey
+          </p>
+
+          <p className="text-[#8B949E] font-[400] text-[20px]">Create, customize, and download your perfect CV—no <br />
+             fees, no hassle, just results. Fast, easy, and tailored for <br />
+              success.</p>
+
+
+            <div className="w-full h-fit flex flex-row items-center gap-7 mt-7 justify-start">
+             <div className="flex flex-row justify-center items-center  w-fit h-fit">
+             <input type="email" name="" id=""  placeholder="Email address" className="placeholder-[#6E7781] text-black p-2 rounded-s-lg"/>
+             <button className="text-center p-[7.3px] bg-black rounded-e-lg border-[1px] border-white ">Sign Up for CV</button>
+             </div>
+
+              <hr className="h-full  border-[0.4px] " />
+              
+              <button className="text-center p-[7.3px] bg-black rounded-lg border-[1px]  border-white ">Sign In → </button>
+              </div>  
+        </div>
+      </div>
     </div>
   );
 }
