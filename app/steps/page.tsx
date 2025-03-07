ada5ec95bde538d4049dacd0e1a8159ac5dc3874
MC_DESMOND <macdesmonddikeibeh@gmail.com>
2025-03-07 23:17:21 +0100

    dvbgbjfgfgtrththtgghfh

--- app/steps/page.tsx
+++ app/steps/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { Div, Center, TextArea } from "../addons/csml"
+import { Div, Center, TextArea,Input } from "../addons/csml"
 import BaseHOC from "../addons/HOC"
 import SliderHOC from "../addons/Slider"
 import React from "react"
@@ -11,9 +11,21 @@ export default function StepPage(){
     var yesCVHaveOne = new BaseHOC<{}>({Component:Center})
     var noCVHaveOne = new BaseHOC<{}>({Component:Center})
     var haveCVDes = new BaseHOC<{}>({Component:Div})
+    var cvFileNameDis = new BaseHOC<{}>({Component:Div})
+    var CVInput = new BaseHOC<{type?:string,hidden?:any}>({Component:(props:any)=><Input {...props}></Input>})
     React.useEffect(()=>{
         haveJobDes.style.display("flex")
         haveCVDes.style.display("flex")
+        CVInput.Execute((el:HTMLInputElement)=>{
+             el.onchange = ()=>{
+
+                if (el.files){
+                    var file = el.files[0]
+                    cvFileNameDis.Execute((dis:HTMLDivElement)=>{
+                        dis.innerText = `${file.name} | ${file.size/(1000*2)}mb | ${file.type}`
+                    })}
+             }
+        })
     })
     return <Center background="rgb(13,17,23)">
         <Div maxWidth="1000px" width="100vw" height="100vh" overflow="auto" display="flex" flexDirection="column" gap="30px" boxSizing="border-box" padding = "20px" paddingTop="30px" >
@@ -61,10 +73,10 @@ export default function StepPage(){
                                         noCVHaveOne.style.border("4px solid rgba(65, 75, 91, 1)")
                                         }} padding ="20px" paddingBlock= "15px"  borderRadius="10px" background="rgb(31,41,55)">No, skip this step</noCVHaveOne.Render>
                                 </Div>
-                                <haveCVDes.Render marginInline="20px" flexDirection="column" gap="20px" display="none" self={haveCVDes}>
+                                <haveCVDes.Render  marginInline="20px" flexDirection="column" gap="20px" display="none" self={haveCVDes}>
                                     <Div fontSize="15px" color="rgba(156, 163, 175, 1)">Upload your CV in PDF or Word format:</Div>
-                                    <input type="file" name="" id="" hidden/>
-                                    <Div paddingBlock = "30px" borderRadius="10px" border="2px solid rgba(65, 75, 91, 1)" display="grid" placeItems="center" gap="10px">
+                                    <CVInput.Render self={CVInput} type="file" id="" hidden/>
+                                    <Div padding = "20px" borderStyle="dashed" onClick={()=>CVInput.Execute((el:HTMLInputElement)=>{el.click();console.log("input clicked")})} paddingBlock = "30px"  borderRadius="10px" border="2px solid rgba(65, 75, 91, 1)" display="grid" placeItems="center" gap="10px">
                                         <Center>
                                             <svg width="50px" height="50px" viewBox="0 0 96 97" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <path d="M84 60.3296V76.3296C84 78.4513 83.1571 80.4862 81.6569 81.9864C80.1566 83.4867 78.1217 84.3296 76 84.3296H20C17.8783 84.3296 15.8434 83.4867 14.3431 81.9864C12.8429 80.4862 12 78.4513 12 76.3296V60.3296" stroke="#6B7280" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
@@ -73,7 +85,7 @@ export default function StepPage(){
                                             </svg>
                                         </Center>
                                         <Div fontSize="15px">Drag and drop your CV here, or click to browse</Div>
-                                        <Div fontSize="14px">Supported formats: PDF, DOC, DOCX</Div>
+                                        <cvFileNameDis.Render self={cvFileNameDis} fontSize="12px">Supported formats: PDF, DOC, DOCX</cvFileNameDis.Render >
                                     </Div>
                                 </haveCVDes.Render>
 
