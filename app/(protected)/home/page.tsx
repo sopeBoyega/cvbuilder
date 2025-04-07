58e91085f13a7aca3e41ea186d2e454a3149f593
s._.ope <sopeadegboyega@gmail.com>
2025-04-07 14:09:51 +0100

    Mobile settings page done

--- app/(protected)/home/page.tsx
+++ app/(protected)/home/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import TextEditor from "@/app/components/home/editor";
 import Header from "@/app/components/home/header";
+import Settings from "@/app/components/home/mobile-settings";
 import RightBar from "@/app/components/home/right-bar";
 import { PJS } from "@/app/fonts/fonts";
 import React from "react";
@@ -15,10 +16,8 @@ const page = (props: Props) => {
 
   return (
     <div className={`${PJS.className} relative`}>
-      <div className="w-full h-full bg-black absolute left-0 right-0 z-[1001] p-3 ">
-        <div className="flex m-3">
-          <p className="font-[500]">Edit Block</p>{" "}
-        </div>
+      <div className="w-full h-full bg-black absolute left-0 right-0 z-[1001] ">
+        <Settings/>
       </div>
       <Header />
       <div className="w-full h-fit flex-col md:flex-row  justify-center items-center gap-3  md:items-start ">
